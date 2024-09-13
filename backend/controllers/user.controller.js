b95cf0e0b897b8254ab2c9752bd54298c842ef33
sakibhasansufi <sakibhasansufi@gmail.com>
2024-09-13 20:16:27 +0600

    follow unfollow added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -115,7 +115,7 @@ export const updateUser = async (req, res) => {
 
 		if (profileImg) {
 			if (user.profileImg) {
-				// https://res.cloudinary.com/dyfqon1v6/image/upload/v1712997552/zmxorcxexpdbh8r0bkjb.png
+				
 				await cloudinary.uploader.destroy(user.profileImg.split("/").pop().split(".")[0]);
 			}
 
--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { useQuery } from "@tanstack/react-query";
+import useFollow from "../hooks/useFollow";
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import LoadingSpinner from "./LoadingSpinner";
 
 const RightPanel = () => {
 	const {data:suggestedUsers,isLoading} = useQuery({
@@ -18,6 +20,8 @@ const RightPanel = () => {
 			}
 		} 
 	});
+
+const {follow,isPending}	= useFollow();
 
 
 	if(suggestedUsers?.length === 0 ) return <div className="md:w-64 w-0"></div>
@@ -59,9 +63,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-gr text-black font-bold hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) =>{
+											e.preventDefault();
+											follow(user._id)
+										}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner size="sm" /> : "Follow"}
 									</button>
 								</div>
 							</Link>
